b35cf28bc028c6a4c22d00c4224b167e03c93e97
Tanuj upadhyay <60139172+TanujUpadhyay@users.noreply.github.com>
2020-08-05 18:16:55 +0530

    Api calling test

--- src/modules/oldAssessment/util/String.tsx
+++
@@ -1,9 +0,0 @@
-export const qustion1 =
-  "1. The arithmetic means of th 5 consective integers starting wiith 's' is 'a. What is the arithematic means of 9 consequtive integers that works with s+22 ?";
-
-export const Q1option1 = "A. 78";
-export const Q1option2 = "B. 58";
-export const Q1option3 = "C. 68";
-export const Q1option4 = "D. 98";
-
-export const Q2option1 = "A. In case otions had a long text";
